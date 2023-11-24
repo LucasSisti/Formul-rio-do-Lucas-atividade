e6814484c2591bb69397e7efcbde073fdd3db690
LucasSisti <150533666+LucasSisti@users.noreply.github.com>
2023-11-23 21:29:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,38 +9,79 @@ app.use(bodyParser.json());
 
 // Função para validar dados do formulário
 function validarDados(dados) {
+    const erros = {};
+
     // Verificar se campos obrigatórios estão preenchidos
-    if (!dados.txtnome || !dados.txtend || !dados.txte-mail || !dados.txtdtnascimento || !dados.txtsenha || !dados.txtsenhaconfirmacao || !dados.opcaosexo || !dados.opcaoanimal) {
-        return 'Todos os campos obrigatórios devem ser preenchidos.';
+    if (!dados.txtnome) {
+        erros.nome = 'Campo obrigatório.';
+    }
+
+    if (!dados.txtend) {
+        erros.endereco = 'Campo obrigatório.';
+    }
+
+    if (!dados.txte_mail) {
+        erros.email = 'Campo obrigatório.';
+    }
+
+    if (!dados.txtdtnascimento) {
+        erros.dataNascimento = 'Campo obrigatório.';
+    }
+
+    if (!dados.txtsenha) {
+        erros.senha = 'Campo obrigatório.';
+    }
+
+    if (!dados.txtsenhaconfirmacao) {
+        erros.senhaConfirmacao = 'Campo obrigatório.';
+    }
+
+    if (!dados.opcaosexo) {
+        erros.sexo = 'Campo obrigatório.';
+    }
+
+    if (!dados.opcaoanimal) {
+        erros.animais = 'Campo obrigatório.';
     }
 
     // Verificar se as senhas estão certas
     if (dados.txtsenha !== dados.txtsenhaconfirmacao) {
-        return 'As senhas não correspondem.';
+        erros.senhaConfirmacao = 'As senhas não correspondem.';
     }
 
     // Verificar o formato da data de nascimento (assumindo formato DD/MM/AAAA)
     const regexDataNascimento = /^\d{2}\/\d{2}\/\d{4}$/;
     if (!regexDataNascimento.test(dados.txtdtnascimento)) {
-        return 'Formato de data de nascimento inválido. Use o formato DD/MM/AAAA.';
+        erros.dataNascimento = 'Formato de data de nascimento inválido. Use o formato DD/MM/AAAA.';
     }
 
     // Verificando o formato do e-mail
     const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!regexEmail.test(dados.txte-mail)) {
-        return 'Formato de e-mail inválido.';
+    if (!regexEmail.test(dados.txte_mail)) {
+        erros.email = 'Formato de e-mail inválido.';
     }
 
-    return null; // Retorna null se não tiver erros 
+    return erros; // Retorna o objeto de erros
 }
 
 app.post('/cadastro', (req, res) => {
     const dados = req.body;
 
     // Validar dados
-    const erroValidacao = validarDados(dados);
-    if (erroValidacao) {
-          return res.send(`Erro de validação: ${erroValidacao}`);
+    const errosValidacao = validarDados(dados);
+    if (Object.keys(errosValidacao).length > 0) {
+        return res.render('seuarquivohtml', {
+            mensagemErroNome: errosValidacao.nome,
+            mensagemErroEndereco: errosValidacao.endereco,
+            mensagemErroEmail: errosValidacao.email,
+            mensagemErroDataNascimento: errosValidacao.dataNascimento,
+            mensagemErroSenha: errosValidacao.senha,
+            mensagemErroSenhaConfirmacao: errosValidacao.senhaConfirmacao,
+            mensagemErroSexo: errosValidacao.sexo,
+            mensagemErroAnimais: errosValidacao.animais,
+            // Passe os dados de formulário de volta para o formulário para manter os valores preenchidos
+            dadosFormulario: dados,
+        });
     }
 
     console.log('Dados do formulário:', dados);
