acf40c951853a8e3430cc4a0d9305e14a2868b82
LucasSisti <150533666+LucasSisti@users.noreply.github.com>
2023-11-25 20:26:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,32 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
 app.post('/cadastro', (req, res) => {
   const nome = req.body.nome;
   const email = req.body.email;
+  const telefone = req.body.telefone;
+  const dataNascimento = req.body.dataNascimento;
+  const genero = req.body.genero;
+  const interesses = req.body.interesses;
 
-  
  
-  res.send(`Cadastro realizado com sucesso! Nome: ${nome}, Email: ${email}`);
+
+ 
+  res.send(`Cadastro realizado com sucesso!
+    Nome: ${nome},
+    Email: ${email},
+    Telefone: ${telefone},
+    Data de Nascimento: ${dataNascimento},
+    Gênero: ${genero},
+    Interesses: ${interesses}`);
 });
 
 // Iniciar o servidor
