1ceac68675eebbde1a23d7dc2f59eda6d67b1589
LucasSisti <150533666+LucasSisti@users.noreply.github.com>
2023-11-25 21:01:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,19 +5,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
 app.post('/cadastro', (req, res) => {
+  // Obtenha os dados do formulário do corpo da solicitação
   const nome = req.body.nome;
   const email = req.body.email;
   const telefone = req.body.telefone;
@@ -25,9 +22,19 @@ app.post('/cadastro', (req, res) => {
   const genero = req.body.genero;
   const interesses = req.body.interesses;
 
- 
+  // Validar os dados do formulário
+  const errors = [];
+  if (!nome) errors.push('Nome é obrigatório.');
+  if (!email || !isValidEmail(email)) errors.push('Email inválido.');
+  if (!telefone || !isValidPhone(telefone)) errors.push('Telefone inválido.');
+  if (!dataNascimento) errors.push('Data de Nascimento é obrigatória.');
 
- 
+  // Se houver erros, renderizar o formulário com mensagens de erro
+  if (errors.length > 0) {
+    return res.status(400).sendFile(path.join(__dirname, 'public', 'index.html'));
+  }
+
+  // Se não houver erros, enviar uma resposta de sucesso
   res.send(`Cadastro realizado com sucesso!
     Nome: ${nome},
     Email: ${email},
@@ -37,7 +44,23 @@ app.post('/cadastro', (req, res) => {
     Interesses: ${interesses}`);
 });
 
-// Iniciar o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
+// Função auxiliar para validar o formato do e-mail
+function isValidEmail(email) {
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+  
+  return /\S+@\S+\.\S+/.test(email);
+}
+
+// Função auxiliar para validar o formato do número de telefone
+function isValidPhone(phone) {
+     const phoneRegex = /^(\d{10}|\d{2}\s\d{4,5}-\d{4})$/;
+    return phoneRegex.test(phone);
+  // Retorna true se o telefone for válido, false caso contrário
+  return /\d{10}/.test(phone);
+}
